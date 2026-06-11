2c64d5e5e14a9cd4a13a8df3722436ca57516658
Teodor Grigor <open-source@teogor.dev>
2026-06-12 02:19:49 +0300

    feat: add CategoryDao and ProductDao for Stitch demo

---
+++ app/src/main/kotlin/dev/teogor/stitch/core/database/dao/CategoryDao.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 teogor (Teodor Grigor)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.teogor.stitch.core.database.dao
+
+import androidx.room3.Dao
+import androidx.room3.Delete
+import androidx.room3.Insert
+import androidx.room3.OnConflictStrategy
+import androidx.room3.Query
+import androidx.room3.Update
+import dev.teogor.stitch.core.database.model.Category
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface CategoryDao {
+  @Query("SELECT * FROM categories")
+  fun getAll(): Flow<List<Category>>
+
+  @Query("SELECT * FROM categories WHERE id = :id")
+  suspend fun getById(id: Long): Category?
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  suspend fun insert(category: Category): Long
+
+  @Update
+  suspend fun update(category: Category)
+
+  @Delete
+  suspend fun delete(category: Category)
+}
---
+++ app/src/main/kotlin/dev/teogor/stitch/core/database/dao/ProductDao.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2024 teogor (Teodor Grigor)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.teogor.stitch.core.database.dao
+
+import androidx.room3.Dao
+import androidx.room3.Delete
+import androidx.room3.Insert
+import androidx.room3.OnConflictStrategy
+import androidx.room3.Query
+import androidx.room3.Update
+import dev.teogor.stitch.ExplicitEntities
+import dev.teogor.stitch.RawOperation
+import dev.teogor.stitch.core.database.model.Category
+import dev.teogor.stitch.core.database.model.Product
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+@ExplicitEntities(
+  entities = [
+    Category::class,
+  ],
+)
+interface ProductDao {
+  @Query("SELECT * FROM products")
+  fun getAll(): Flow<List<Product>>
+
+  @Query("SELECT * FROM products WHERE id = :id")
+  suspend fun getById(id: Long): Product?
+
+  @RawOperation
+  @Query("SELECT * FROM products WHERE name LIKE '%' || :query || '%'")
+  fun searchProducts(query: String): Flow<List<Product>>
+
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  suspend fun insert(product: Product): Long
+
+  @Update
+  suspend fun update(product: Product)
+
+  @Delete
+  suspend fun delete(product: Product)
+}
