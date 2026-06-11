c409747f16e74e078c9cf6f22d0eb6dfed0c3901
Teodor Grigor <open-source@teogor.dev>
2026-06-12 02:14:15 +0300

    Fix MainActivity: fix typo in stitchTheme and add missing import

--- app/src/main/kotlin/dev/teogor/stitch/MainActivity.kt
+++ app/src/main/kotlin/dev/teogor/stitch/MainActivity.kt
@@ -26,12 +26,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import dev.teogor.stitch.ui.theme.stitchTheme
 
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
-      StitchTheme {
+      stitchTheme {
         // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier.fillMaxSize(),
