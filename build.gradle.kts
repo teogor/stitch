d9592f29fcd4a7f5d5956c19883eb74e2a8b7ab4
Teodor Grigor <open-source@teogor.dev>
2026-06-12 06:18:40 +0300

    feat(catalog): initialize webApp module

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ import org.jetbrains.dokka.gradle.DokkaPlugin
 plugins {
   alias(libs.plugins.android.application) apply false
   alias(libs.plugins.jetbrains.kotlin.android) apply false
+  alias(libs.plugins.jetbrains.kotlin.multiplatform) apply false
   alias(libs.plugins.jetbrains.kotlin.jvm) apply false
   alias(libs.plugins.jetbrains.compose.compiler) apply false
   alias(libs.plugins.metro) apply false
