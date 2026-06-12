693e0ebbb75172e1425017480572ac7c180bf26b
Teodor Grigor <open-source@teogor.dev>
2026-06-12 17:32:49 +0300

    chore(catalog): apply quality gate fixes

--- catalog/shared/src/jsMain/kotlin/dev/teogor/stitch/catalog/Platform.js.kt
+++ catalog/shared/src/jsMain/kotlin/dev/teogor/stitch/catalog/Platform.js.kt
@@ -1,14 +1,30 @@
+/*
+ * Copyright 2026 teogor (Teodor Grigor)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package dev.teogor.stitch.catalog
 
 import web.navigator.navigator
 
-class JsPlatform: Platform {
-    private val userAgent = navigator.userAgent
-    private val browserList = listOf("Chrome", "Firefox", "Safari", "Edge")
+class JsPlatform : Platform {
+  private val userAgent = navigator.userAgent
+  private val browserList = listOf("Chrome", "Firefox", "Safari", "Edge")
 
-    override val name: String = userAgent.findAnyOf(browserList, ignoreCase = true)
-            ?.let { (startIndex) -> userAgent.substring(startIndex).substringBefore(" ") }
-            ?: "Unknown"
+  override val name: String = userAgent.findAnyOf(browserList, ignoreCase = true)
+    ?.let { (startIndex) -> userAgent.substring(startIndex).substringBefore(" ") }
+    ?: "Unknown"
 }
 
 actual fun getPlatform(): Platform = JsPlatform()
